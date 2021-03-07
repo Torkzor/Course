2d59dd6a8aa1472fd22d4f896b2c85c1bd36cb17
Jorge <jorgearthurs@hotmail.com>
2021-03-07 19:31:56 -0300

    Exercicio

---
+++ Order/Program.cs
@@ -0,0 +1,55 @@
+﻿using Order.Entities.Enums;
+using Order.Entities;
+using System;
+using System.Globalization;
+
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine("How many Items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            
+            Client client = new Client(clientName, email, birthDate);
+            Entities.Order order = new Entities.Order(DateTime.Now, status, client);
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item  data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, price);
+                
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+        }
+    }
+}
